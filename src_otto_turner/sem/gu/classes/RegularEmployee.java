9b93e7332d07228e8108a242d285958175a8dfbc
Jack David Turner <up776193@myport.ac.uk>
2018-10-06 19:19:41 +0200

    reverted the revert, wasnt allowing the creation of employees or its children

--- src_otto_turner/sem/gu/classes/RegularEmployee.java
+++ src_otto_turner/sem/gu/classes/RegularEmployee.java
@@ -2,9 +2,9 @@ package sem.gu.classes;
 
 public class RegularEmployee extends Employee {
 
-    Employee(String name, double grossSalary) {
+	public RegularEmployee(String name, double grossSalary) {
         super(name, grossSalary);
-        taxingScheme = StandarTax;      //TODO: implement this
+        //taxingScheme = StandarTax;      //TODO: implement this
     }
 
     //TODO: further functionalities of StandardEmployee
