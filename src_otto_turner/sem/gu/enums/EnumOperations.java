9742dc2eca0b619936b492eb9e84514604cc3368
Konrad Otto <gusottko@student.gu.se>
2018-10-11 10:54:25 +0200

    Cast string to enum ignoring case.

--- src_otto_turner/sem/gu/enums/EnumOperations.java
+++ src_otto_turner/sem/gu/enums/EnumOperations.java
@@ -1,4 +1,17 @@
 package sem.gu.enums;
 
+/**
+ *  Class providing common enum operations statically
+ */
 public class EnumOperations {
+
+    public static <T extends Enum<?>> T searchEnum(Class<T> enumeration,
+                                                   String search) {
+        for (T each : enumeration.getEnumConstants()) {
+            if (each.name().compareToIgnoreCase(search) == 0) {
+                return each;
+            }
+        }
+        return null;
+    }
 }
