dc811bc26d79264d2709865aa62a6322c7689e31
Konrad Otto <gusottko@student.gu.se>
2018-10-13 19:53:28 +0200

    add Manager Salary

---
+++ src_otto_turner/sem/gu/classes/InternSalary.java
@@ -0,0 +1,16 @@
+package sem.gu.classes;
+
+public class InternSalary implements SalaryScheme {
+
+    private static final
+
+    @Override
+    public double calculateGrossTotal(double basicGrossSalary) {
+        return 0;
+    }
+
+    @Override
+    public double calculateNetSalary(double basicGrossSalary) {
+        return 0;
+    }
+}
---
+++ src_otto_turner/sem/gu/exceptions/IllegalGPAException.java
@@ -0,0 +1,8 @@
+package sem.gu.exceptions;
+
+public class IllegalGPAException extends Exception {
+
+    public IllegalGPAException(String message) {
+        super(message);
+    }
+}
